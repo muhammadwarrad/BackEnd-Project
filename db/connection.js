a3bf2caa8597862e2103a4339a532f2f0988eed3
muhammadwarrad <moe_warrad@yahoo.com>
2023-01-23 14:59:18 -0500

    comments added for all files part2

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,23 @@
-//Imports
+// Imports
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+
+// Configure .env
 dotenv.config()
 
-//Configure
+// Configure
 let mongooseConnectionConfig = { useNewUrlParser: true, useUnifiedTopology: true}
 mongoose.set('strictQuery', true)
 
-//Assign URL
+// Assign URL
 const url = process.env.MONGO_URL || 'mongodb://localhost:27017/ProductData'
 
-//Mongoose Connect
+// Mongoose Connect
 mongoose.connect(url, mongooseConnectionConfig)
 
 mongoose.connection.on('connected', ()=> console.log("Connected to database"))
 mongoose.connection.on('disconnected', ()=> console.log("Disconnected from database"))
 mongoose.connection.on('error', error=> console.error("Database error", error))
 
-//Export
+// Export
 export default mongoose.connection
