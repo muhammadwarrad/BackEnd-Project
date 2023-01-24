57eac5b8d4788f1e761669e9d79193276f8d78fe
bryxnt1 <bryantperez56@gmail.com>
2023-01-24 11:44:46 -0500

    updated user model

--- models/Users.js
+++ models/Users.js
@@ -6,10 +6,9 @@ const Schema = mongoose.Schema;
 // Schema for Users
 let Users = new Schema({
     "username": {type: String, required: true},
-    "password": {type: String, required: true},
     "email": {type: String, required: true},
-    "cart": [{type: Object}],
-    "loggedIn": {type: Boolean}
+    "password_digest": {type: String, required: true, select: false},
+    "cart": [{type: Object}]
 });
 // Export Users Schema
 export default mongoose.model("users", Users);
