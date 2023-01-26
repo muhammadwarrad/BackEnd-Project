34c9ae1628a244a5b0184644c22a4bded3e6cfd0
jagerziel <fromnytosg@gmail.com>
2023-01-26 15:18:07 -0500

    Fixing CORS

--- server.js
+++ server.js
@@ -15,8 +15,13 @@ const PORT = process.env.PORT || process.env.LOCAL_PORT;
 
 // Uses
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/", routes);
-app.use(cors)
+
 
 // Db connection
 db.on("connected", () => {
